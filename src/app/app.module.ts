fd441f70b8dc1af8943eac7860c70d05dce0c2cf
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-12-14 21:52:26 +0530

    h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { DialogComponent } from './dialog/dialog.component';
-import { AccordianComponent } from './accordian/accordian.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    DialogComponent,
-    AccordianComponent
   ],
   imports: [
     BrowserModule,
